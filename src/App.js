ca2a09621f946309a905d4c5f431d605cebc13e7
Damjan Banjac <damjanbanjac@Damjans-MacBook-Pro.local>
2022-05-27 13:24:39 +0200

    Add deploy config

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import ReactDOM from 'react-dom';
 
 import Button from '@mui/material/Button';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBamRPS6NxxPg7lplFrxZd36fLWg9ZUTDY",
+  authDomain: "ambient-systems.firebaseapp.com",
+  databaseURL: "https://ambient-systems-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ambient-systems",
+  storageBucket: "ambient-systems.appspot.com",
+  messagingSenderId: "1001740093061",
+  appId: "1:1001740093061:web:80b8003e2dfed714b90bec"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 
 function App() {
 
